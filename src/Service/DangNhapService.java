53e594fec921e15d37b760f0a5fbd4f96454c685
TienBB303 <tienbbph31709@fpt.edu.vn>
2024-03-21 17:35:07 +0700

    tien

---
+++ src/Service/DangNhapService.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+import Repository.DbConnect;
+import java.sql.*;
+
+/**
+ *
+ * @author TienBB
+ */
+public class DangNhapService {
+
+    private Connection cn;
+
+    public DangNhapService() {
+        try {
+            cn = DbConnect.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean kiemTraDangNhap(String tenDangNhap, String matKhau) {
+        try {
+            String query = "SELECT * FROM TaiKhoan WHERE TenDangNhap = ? AND MatKhau = ?";
+            PreparedStatement ps = cn.prepareStatement(query);
+            ps.setString(1, tenDangNhap);
+            ps.setString(2, matKhau);
+            ResultSet rs = ps.executeQuery();
+            return rs.next(); // Trả về true nếu có bất kỳ kết quả nào từ truy vấn
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
