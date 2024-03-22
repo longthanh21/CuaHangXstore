afa95c15d59ec9ac132c5f3f96667e996a1bf72c
TienBB303 <tienbbph31709@fpt.edu.vn>
2024-03-22 23:07:50 +0700

    tien

--- src/Service/QuanLyKhuyenMai.java
+++ src/Service/QuanLyKhuyenMai.java
@@ -8,11 +8,14 @@ import Model.Voucher;
 import Repository.DbConnect;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author KhanhCT
  */
 public class QuanLyKhuyenMai {
+
     private Connection cn;
 
     public QuanLyKhuyenMai() {
@@ -22,22 +25,22 @@ public class QuanLyKhuyenMai {
             e.printStackTrace();
         }
     }
-    
-    public java.util.List getAllVC(){
+
+    public List<Voucher> getAllVC() {
         ArrayList<Voucher> listvc = new ArrayList<>();
         try {
             String sql = "SELECT * FROM Voucher";
             PreparedStatement ps = cn.prepareStatement(sql);
             ps.execute();
             ResultSet rs = ps.getResultSet();
-            while(rs.next()){
+            while (rs.next()) {
                 Voucher vc = new Voucher();
-                        vc.setMaVC(rs.getString("MaVC"));
-                        vc.setTenVC(rs.getString("TenVC"));
-                        vc.setGiamGia(rs.getString("GiamGia"));
-                        vc.setNgayBatDau(rs.getString("NgayBatDau"));
-                        vc.setNgayKetThuc(rs.getString("NgayKetThuc"));
-                        vc.setTrangThai(rs.getString("TrangThai"));
+                vc.setMaVC(rs.getString("MaVC"));
+                vc.setTenVC(rs.getString("TenVC"));
+                vc.setGiamGia(rs.getString("GiamGia"));
+                vc.setNgayBatDau(rs.getString("NgayBatDau"));
+                vc.setNgayKetThuc(rs.getString("NgayKetThuc"));
+                vc.setTrangThai(rs.getString("TrangThai"));
                 listvc.add(vc);
             }
         } catch (Exception e) {
@@ -45,6 +48,21 @@ public class QuanLyKhuyenMai {
         }
         return listvc;
     }
-    
-    
+
+    public void themVoucher(Voucher vc) {
+        try {
+            String sql = "INSERT INTO Voucher" + "VALUES(?,?,?,?,?,?)";
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setString(1, vc.getMaVC());
+            ps.setString(2, vc.getTenVC());
+            ps.setString(3, vc.getGiamGia());
+            ps.setString(4, vc.getNgayBatDau());
+            ps.setString(5, vc.getNgayKetThuc());
+            ps.setString(6, vc.getTrangThai());
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
