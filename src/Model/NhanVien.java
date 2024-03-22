30222458bda610c88121e3d2c08cb002389e425b
Your Name <you@example.com>
2024-03-22 15:13:56 +0700

    khanh

--- src/Model/NhanVien.java
+++ src/Model/NhanVien.java
@@ -9,5 +9,112 @@ package Model;
  * @author KhanhCT
  */
 public class NhanVien {
+    private String maNV;
+    private String tenNV;
+    private String ngaySinh;
+    private boolean gioiTinh;
+    private String diaChi;
+    private String SĐT;
+    private boolean vaiTro;
+    private String maTK;
+    private String maCL;
+    private boolean trangThai;
+
+    
+
+    public NhanVien(String maNV, String tenNV, String ngaySinh, boolean gioiTinh, String diaChi, String SĐT, boolean vaiTro, String maTK, String maCL, boolean trangThai) {
+        this.maNV = maNV;
+        this.tenNV = tenNV;
+        this.ngaySinh = ngaySinh;
+        this.gioiTinh = gioiTinh;
+        this.diaChi = diaChi;
+        this.SĐT = SĐT;
+        this.vaiTro = vaiTro;
+        this.maTK = maTK;
+        this.maCL = maCL;
+        this.trangThai = trangThai;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getTenNV() {
+        return tenNV;
+    }
+
+    public void setTenNV(String tenNV) {
+        this.tenNV = tenNV;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public boolean isGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(boolean gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getSĐT() {
+        return SĐT;
+    }
+
+    public void setSĐT(String SĐT) {
+        this.SĐT = SĐT;
+    }
+
+    public boolean isVaiTro() {
+        return vaiTro;
+    }
+
+    public void setVaiTro(boolean vaiTro) {
+        this.vaiTro = vaiTro;
+    }
+
+    public String getMaTK() {
+        return maTK;
+    }
+
+    public void setMaTK(String maTK) {
+        this.maTK = maTK;
+    }
+
+    public String getMaCL() {
+        return maCL;
+    }
+
+    public void setMaCL(String maCL) {
+        this.maCL = maCL;
+    }
+
+    public boolean isTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        this.trangThai = trangThai;
+    }
+    
+    
     
 }
