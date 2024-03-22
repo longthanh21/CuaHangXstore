4b4d29c9cc020a24b4943d7cdd4fb29724f82c45
Nguyen Van Sao <saonvph45620@fpt.fptedu.vn>
2024-03-22 18:08:48 +0700

    sao

---
+++ src/Model/Voucher.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author TienBB
+ */
+public class Voucher {
+
+    private String maVC, tenVC, giamGia, ngayBatDau, ngayKetThuc, trangThai;
+
+    public Voucher() {
+    }
+
+    public Voucher(String maVC, String tenVC, String giamGia, String ngayBatDau, String ngayKetThuc, String trangThai) {
+        this.maVC = maVC;
+        this.tenVC = tenVC;
+        this.giamGia = giamGia;
+        this.ngayBatDau = ngayBatDau;
+        this.ngayKetThuc = ngayKetThuc;
+        this.trangThai = trangThai;
+    }
+
+    public String getMaVC() {
+        return maVC;
+    }
+
+    public void setMaVC(String maVC) {
+        this.maVC = maVC;
+    }
+
+    public String getTenVC() {
+        return tenVC;
+    }
+
+    public void setTenVC(String tenVC) {
+        this.tenVC = tenVC;
+    }
+
+    public String getGiamGia() {
+        return giamGia;
+    }
+
+    public void setGiamGia(String giamGia) {
+        this.giamGia = giamGia;
+    }
+
+    public String getNgayBatDau() {
+        return ngayBatDau;
+    }
+
+    public void setNgayBatDau(String ngayBatDau) {
+        this.ngayBatDau = ngayBatDau;
+    }
+
+    public String getNgayKetThuc() {
+        return ngayKetThuc;
+    }
+
+    public void setNgayKetThuc(String ngayKetThuc) {
+        this.ngayKetThuc = ngayKetThuc;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+}
